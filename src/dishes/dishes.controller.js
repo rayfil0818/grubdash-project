23fda58d6e0cccdc4432425a48ff3151ba9708d8
Raymond Filipinia <rfilipinia@indeed.com>
2024-08-23 17:59:08 +0900

    First changes in dishes.* files.

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
+
 // Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
 
@@ -7,3 +8,107 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function priceIsValidNumber(req, res, next) {
+    const { data: { price } = {} } = req.body;
+    if (price <= 0 || !Number.isInteger(price)) {
+      return next({
+        status: 400,
+        message: `Dish must have a price that is an integer greater than 0`,
+      });
+    }
+    next();
+  }
+
+function bodyDataHas(propertyName) {
+    return function (req, res, next) {
+      const { data = {} } = req.body;
+      if (data[propertyName]) {
+        return next();
+      }
+      next({ status: 400, message: `Dish must include a ${propertyName}` });
+    };
+  }
+
+function idInBodyMatchRouteId(req, res, next) {
+    const { dishId } = req.params;
+    const { data: { id } = {} } = req.body;
+    if (dishId === id) {
+      return next();
+    }
+    next({
+        status: 404,
+        message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
+      });
+}
+
+function dishExists (req, res, next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id === Number(dishId));
+    if (foundDish) {
+      res.locals.dish = foundDish;
+      return next();
+    }
+    next({
+      status: 404,
+      message: `Dish does not exist: ${dishId}`,
+    });
+}
+
+function create (req, res, next) {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    const newDish = {
+      id: nextId(),
+      name,
+      description,
+      price,
+      image_url,
+    };
+    urls.push(newDish);
+    res.status(201).json({ data: newDish });
+}
+
+function read (req, res, next) {
+    res.json({ data: res.locals.dish });
+}
+
+function update (req, res, next) {
+    const dish = res.locals.dish;
+    const { data: { href } = {} } = req.body;
+  
+    // Update the url
+    url.href = href;
+  
+    console.log(url);
+  
+    res.json({ data: url });
+}
+
+function list (req, res, next) {
+    res.json({ data: dishes });
+}
+
+module.exports = {
+    list,
+    read: [
+        dishExists,
+        read
+    ],
+    create: [
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValidNumber,
+        create,
+    ],
+    update: [
+        idInBodyMatchRouteId,
+        dishExists,
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValidNumber,
+        update,
+    ],
+};
